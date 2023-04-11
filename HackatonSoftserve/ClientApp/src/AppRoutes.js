1081f773b772f9f45586cede2b7b2cc776ebf2cc
Maks Frang <deneka.mn@gmail.com>
2023-04-11 13:10:42 +0300

    Authorization

--- HackatonSoftserve/ClientApp/src/AppRoutes.js
+++ HackatonSoftserve/ClientApp/src/AppRoutes.js
@@ -1,15 +1,21 @@
 // import Navbar from "./components/NavMenu/NavMenu";
-import Home from "./components/Home/Home";
+import Home from './components/Home/Home';
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
 
 const AppRoutes = [
   {
     index: true,
-    element: <Home />
+    element: <Home />,
   },
-  // {
-  //   path: '/navbar',
-  //   element: <Navbar />
-  // }
+  {
+    path: '/signin',
+    element: <SignIn />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
+  },
 ];
 
 export default AppRoutes;
--- HackatonSoftserve/ClientApp/src/components/NavMenu/NavMenu.js
+++ HackatonSoftserve/ClientApp/src/components/NavMenu/NavMenu.js
@@ -6,29 +6,35 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { useNavigate } from 'react-router-dom';
 
 const NavMenu = () => {
+  const navigate = useNavigate();
+
+  function goToLogin() {
+    navigate('/signin');
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Home
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">
+            Chart
+          </Button>
+          <Button onClick={goToLogin} color="inherit">	
+            Login
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
   );
-}
+};
 
-export default NavMenu
+export default NavMenu;
