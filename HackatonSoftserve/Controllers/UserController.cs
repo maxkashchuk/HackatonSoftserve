fc485e7483784384f6d0c9fa5d66a51288bcaa45
Maksym <kashchukmaksym2003258@gmail.com>
2023-04-11 19:33:50 +0300

    Authentication made

--- HackatonSoftserve/Controllers/UserController.cs
+++ HackatonSoftserve/Controllers/UserController.cs
@@ -5,55 +5,94 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HackatonSoftserve.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
     public class UserControlelr : ControllerBase
     {
-        ApplicationContext _AppContext;
 
-        public UserControlelr(ApplicationContext _AppContext)
+        private readonly ILogger<UserControlelr> _logger;
+
+        public UserControlelr(ILogger<UserControlelr> logger)
         {
-            this._AppContext = _AppContext;
+            _logger = logger;
         }
 
-        [Route("signin")]
-        [HttpPost]
-        public ActionResult SignIn(SignInUser user)
+        [Route("example")]
+        [HttpGet]
+        public ActionResult Example()
         {
-            if(user != null)
-            {
-                User? u = _AppContext.Users.Where(el => el.Email == user.Email || el.Password == user.Password).FirstOrDefault();
-                if(u != null)
-                {
-                    return Ok(u);
+            return Ok("All is good");
                 }
-                return BadRequest();
             }
-            return BadRequest();
         }
 
-        [Route("signup")]
-        [HttpPost]
-        async public Task<ActionResult> SignUp(SignUpUser user)
-        {            
-            if(user != null)
-            {
-                User u = new User()
-                {
-                    Name = user.Name,
-                    Surname = user.Surname,
-                    Email = user.Email,
-                    Password = user.Password,
-                    Role = user.Role,
-                    Faculty = user.Faculty,
-                    Group = user.Group,
-                    Image = user.Image,
-                };
-                _AppContext.Users.Add(u);
-                await _AppContext.SaveChangesAsync();
-                return Ok(u);
-            }
-            return BadRequest();
-        }
-    }
-}
+
+
+//using HackatonSoftserve.Models;
+//using HackatonSoftserve.Models.AuthModels;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace HackatonSoftserve.Controllers
+//{
+//    [ApiController]
+//    [Route("api/[controller]")]
+//    public class UserControlelr : ControllerBase
+//    {
+//        ApplicationContext _AppContext;
+
+//        private readonly ILogger<TestController> _logger;
+
+//        public UserControlelr(ApplicationContext _AppContext, ILogger<TestController> logger)
+//        {
+//            this._AppContext = _AppContext;
+//            _logger = logger;
+//        }
+
+//        [Route("signin")]
+//        [HttpPost]
+//        public ActionResult SignIn(SignInUser user)
+//        {
+//            if(user != null)
+//            {
+//                User? u = _AppContext.Users.Where(el => el.Email == user.Email || el.Password == user.Password).FirstOrDefault();
+//                if(u != null)
+//                {
+//                    return Ok(u);
+//                }
+//                return BadRequest();
+//            }
+//            return BadRequest();
+//        }
+
+//        [Route("signup")]
+//        [HttpPost]
+//        async public Task<ActionResult> SignUp(SignUpUser user)
+//        {            
+//            if(user != null)
+//            {
+//                User u = new User()
+//                {
+//                    Name = user.Name,
+//                    Surname = user.Surname,
+//                    Email = user.Email,
+//                    Password = user.Password,
+//                    Role = user.Role,
+//                    Faculty = user.Faculty,
+//                    Group = user.Group,
+//                    Image = user.Image,
+//                };
+//                _AppContext.Users.Add(u);
+//                await _AppContext.SaveChangesAsync();
+//                return Ok(u);
+//            }
+//            return BadRequest();
+//        }
+
+//        [Route("example")]
+//        [HttpGet]
+//        async public Task<ActionResult> Example()
+//        {
+//            return Ok();
+//        }
+//    }
+//}
